abd14275456d331d84944416d64a1fb1557054fa
ramadvs <rsunkavalli18@gmail.com>
2021-05-04 21:59:39 -0700

    message2

--- SeleniumBasics/src/main/java/ActionsClass/DragAndDropConcept.java
+++ SeleniumBasics/src/main/java/ActionsClass/DragAndDropConcept.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 public class DragAndDropConcept {
 
 	public static void main(String[] args) {
+		//adding comment1
 		
 System.setProperty("webdriver.chrome.driver","C:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
 		
@@ -23,7 +24,7 @@ System.setProperty("webdriver.chrome.driver","C:\\ChromeDriver\\chromedriver_win
 
 		act.clickAndHold(Source1).moveToElement(Target1).release().build().perform();
 		
-		//act.dragAndDrop(Source1,Target1 ).build().perform();
+		//act.dragAndDrop(Source1,Target1 ).build().perform(); 
 		
 		driver.close();
 	}
